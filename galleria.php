d4860c34507036f75eaad207500422c0cf2e7f5c
Josh <JPollock412@gmail.com>
2013-07-11 00:57:04 -0400

    debug and otherwise make work galleria

--- galleria.php
+++ galleria.php
@@ -6,7 +6,11 @@
 *
 */
 
-//add galleria js
+/**
+* Add Galleria JS/CSS
+*
+*@since _sf-1.1.0
+*/
 
 function _sf_scripts_galleria() {
 	wp_enqueue_script('galleria', get_template_directory_uri().'/lib/js/galleria-1.2.9.min.js', array('jquery'), false, true);
@@ -14,28 +18,60 @@ function _sf_scripts_galleria() {
 }
 add_action('wp_enqueue_scripts', '_sf_scripts_galleria', 10);
 
-//do that good voodoo that you do
-add_action('wp_footer', '_sf_galleriaSetup');
-function _sf_galleriaSetup() {
-	//get the tree image categories, and put their IDs into array $cats and individaul vars
-	$options = get_option('option_tree');
-	$cat1 = $options[ 'filter_cat_1'];
-	$cat2 = $options[ 'filter_cat_2'];
-	$cat3 = $options[ 'filter_cat_3'];
-	$cats = array ($cat1, $cat2, $cat3);
+/**
+* Galleria CSS
+*/
+function _sf_galleriaCSS() {
+	echo "<style> #galleria{ width: 100%; height: 500px; background: #000; } </style>";
+}
+add_action('wp_head', '_sf_galleriaCSS');
 
+/**
+* Setup galleria and data for it
+*
+*@since _sf-1.1.0
+*
+*@params $default (required) first category in main dataset. Only on if $combine == false.
+*@params $cats (required) array of category IDs to create datasets for. NOT including the default category.
+*@params $combine (optional) (default = true). If set to true all data sets will be combined into one, big one, which will be default.
+*@params $switches (optional) (default = true) If set to true, will create the ability to switch categories via jQuery onClick("#switch-x")
+* 
+*@params $desc, $link, false see _sf_galleria_data below
+* $combine = true, $switches = true, $title = false, $desc = false, $link = false
+*/
+add_action('wp_footer', '_sf_galleriaSetup', 10000);
+//*this should be gotten from options*/
+$default = 5;
+$cats = array(8, 6);
+
+function _sf_galleriaSetup($default, $cats = array()) {
+$combine = true;
 	/*Start and wrap script */
 	echo "<script> \n ";
 	echo "jQuery(document).ready(function($) { \n ";
 	
 	/*CONSTRUCT DATA SETS */
+	//defualt first
+		_sf_galleria_data($default);
 	foreach ($cats as $CatID) {
 		_sf_galleria_data($CatID);
 	}
 	
-	//create var data with all house pictures.
-	echo "var data = data".$cat1.".concat(data".$cat2.", data".$cat3."); \n ";
-	
+	//create var data with all of them (if combine = true) and set $defaultData to "data"
+	//else skip concat and set $defaultData to data. the id of the default category.
+	if ($combine == true) {
+			echo "var data = data".$default.".concat(";
+			
+			foreach ($cats as $cat) {
+				echo "data".$cat.", ";
+			}
+			
+			echo ");";
+			$defaultData = "data";
+	}
+	else {
+		$defaultData = "data".$default;
+	}
 	/*Setup Galleria*/
 	//but first put location of theme js in a $themejs
 	$themejs = get_template_directory_uri().'/lib/js/galleria.classic.min.js';
@@ -56,7 +92,7 @@ function _sf_galleriaSetup() {
 			Galleria.configure({
 				thumbnails:'lazy',
 				lightbox: true,
-				dataSource: data,
+				dataSource: ".$defaultData.",
 				responsive:true,
 				height:0.9,
 				//debug:false
@@ -76,14 +112,13 @@ function _sf_galleriaSetup() {
 				//play/pause toggle
 					extend: function() {
 						var gallery = this; 
-						$('#g-play-pause').click(function() {
+						$('#galleria-play-pause').click(function() {
 							gallery.playToggle()
 						});
 		";
 		//create switches for filter categories
 		$x=0;
 		foreach ($cats as $CatID) {
-			if ($x==3) break;
 			$switches = "
 					 $('#switch-".$x."').click(function() {
 						gallery.load(data".$CatID.");
@@ -95,9 +130,9 @@ function _sf_galleriaSetup() {
 			echo $switches;
 			$x++;
 		}
-		//create #all-photos switch for switch back to complete set (data) in main menu
+		//create #all switch for switch back to complete set (data) in main menu
 		echo "
-				 $('#all-photos').click(function() {
+				 $('#all').click(function() {
 					gallery.load(data);
 					window.setTimeout(function(){
 						gallery.lazyLoadChunks(3);
@@ -117,7 +152,16 @@ function _sf_galleriaSetup() {
 	echo " \n </script>";
 }
 
-//function to create the data sets.
+/**
+* Function to create the data sets.
+*
+*@since _sf-1.1.0
+*
+*@params $CatID (required) ID of categories to create data sets for.
+*@params $title, $desc, $link (optional) (default false) If true add title (the_title()), description (the_excerpt()) and link (the_permalink()) to the datas
+/* $title = false, $desc = false, $link = false
+*/
+
 function _sf_galleria_data($CatID) {
 	//setup before and after for data sets
 	$startData = "var data".$CatID." = [ \n ";
@@ -150,9 +194,9 @@ function _sf_galleria_data($CatID) {
 		$out .= "thumb: '".$thumb[0]."', \n ";
 		$out .= "big: '".$full[0]."', \n ";
 		$out .= "title: '".$title."', \n ";
-		//$out .= "description: '".$desc."', \n ";
+		$out .= "description: '".$desc."', \n ";
 		//$out .= "link: '".$link."', 
-		//";
+		
 	   $out .= "}, \n ";
 	   //Kirk: ECHO!
 	   echo $out;
